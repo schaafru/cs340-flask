cc041b6ed6ff45ce0fb07224ddbee8c0c3803500
schaafru <schaafru@oregonstate.edu>
2022-05-20 18:32:58 -0700

    All entities should be able to add rows to tables

--- app.py
+++ app.py
@@ -25,31 +25,74 @@ def home():
     return render_template('Home.html')
 
 
-@webapp.route('/weapon')
+@webapp.route('/weapon', methods=['POST', 'GET'])
 def weapon():
     cursor =  db_conn.cursor()
-    cursor.execute("SELECT weapon_ID, order_ID, price FROM Weapon;")
-    r = cursor.fetchall()
-    print(r)
-    return render_template('Weapon.html', rows=r)
+
+    if request.method == "GET":
+        cursor.execute("SELECT weapon_ID, order_ID, price FROM Weapon;")
+        r = cursor.fetchall()
+        print(r)
+        return render_template('Weapon.html', rows=r)
+
+    elif request.method == "POST":
+        cursor = db_conn.cursor()
+        details = request.form
+        currency = details['currency']
+        customer_ID = details['customer_ID']
+        order_date = details['order_date']
+        order_ID = details['order_ID']
+        weapon_ID = details['weapon_ID']
+        data = (currency, customer_ID, order_date, order_ID, weapon_ID)
+        cursor.execute("INSERT INTO Weapon (currency, customer_ID, order_date, order_ID, weapon_ID) VALUES "
+                       "(%s, %s, %s, %s, %s)", data)
+        return render_template('Weapon.html')
 
 
-@webapp.route('/orders')
+@webapp.route('/orders', methods=['POST', 'GET'])
 def orders():
     cursor = db_conn.cursor()
-    cursor.execute("SELECT order_ID, customer_ID, weapon_ID, currency, order_date FROM Orders;")
-    r = cursor.fetchall()
-    print(r)
-    return render_template('Orders.html', rows=r)
+
+    if request.method == "GET":
+        cursor.execute("SELECT order_ID, customer_ID, weapon_ID, currency, order_date FROM Orders;")
+        r = cursor.fetchall()
+        print(r)
+        return render_template('Orders.html', rows=r)
+
+    elif request.method == "POST":
+        cursor = db_conn.cursor()
+        details = request.form
+        order_ID = details['order_ID']
+        price = details['price']
+        weapon_ID = details['weapon_ID']
+        currency = details['currency']
+        order_date = details['order_date']
+        data = (order_ID, price, weapon_ID, currency, order_date)
+        cursor.execute("INSERT INTO Orders (order_ID, price, weapon_ID) VALUES (%s, %s, %s, %s, %s)", data)
+        return render_template('Orders.html')
 
 
-@webapp.route('/stock')
+@webapp.route('/stock', methods=['POST', 'GET'])
 def stock():
     cursor = db_conn.cursor()
-    cursor.execute("SELECT total_number_of_items, number_of_items_available, weapon_ID, weapon_type FROM Stock;")
-    r = cursor.fetchall()
-    print(r)
-    return render_template('Stock.html', rows=r)
+
+    if request.method == "GET":
+        cursor.execute("SELECT number_of_items_available, total_number_of_items, weapon_ID, weapon_type FROM Stock;")
+        r = cursor.fetchall()
+        print(r)
+        return render_template('Stock.html', rows=r)
+
+    elif request.method == "POST":
+        cursor = db_conn.cursor()
+        details = request.form
+        number_of_items_available = details['number_of_items_available']
+        total_number_of_items = details['total_number_of_items']
+        weapon_ID = details['weapon_ID']
+        weapon_type = details['weapon_type']
+        data = (number_of_items_available, total_number_of_items, weapon_ID, weapon_type)
+        cursor.execute("INSERT INTO Stock (number_of_items_available, total_number_of_items, weapon_ID, weapon_type) "
+                       "VALUES (%s, %s, %s, %s)", data)
+        return render_template('Stock.html')
 
 
 @webapp.route('/customer', methods=['POST', 'GET'])
@@ -71,6 +114,7 @@ def customer():
         data = (name, address, customer_ID)
         cursor.execute("INSERT INTO Customer (name, address, customer_ID) VALUES (%s, %s, %s)", data)
         return render_template('Customer.html')
+
 
 if __name__ == '__main__':
     webapp.run()
